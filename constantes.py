374dceba332735c201de2ab72b0df80dcde87739
raphaelpassos <raphael_magalhaes@live.com>
2022-08-18 00:47:00 -0300

    windowStart

--- constantes.py
+++ constantes.py
@@ -10,10 +10,16 @@ FPS = 30
 
 #Cores
 PRETO = (0, 0, 0)
+BRANCO = (255, 255, 255)
+AMARELO = (244, 233, 51)
 
-#Imagens
+#Images
 SPRITESHEET = 'spritesheet.png'
 PACMAN_START_LOGO = 'pacman-logo-1.png'
 
 #Fonte
 FONTE = 'arial'
+
+#Audios
+MUSICA_START = 'intermission.wav'
+TECLA_START = 'munch_1.wav'
